07a91f1167cac3c6b5bb0c82619af9dd80c5a037
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2026-03-14 11:00:42 +0700

    refactor: set up default rotation angle, and simpilfy road creating methods by changing the angle before swapping model

--- Assets/Scripts/RoadFixer.cs
+++ Assets/Scripts/RoadFixer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RoadFixer : MonoBehaviour
 {
     public GameObject deadEnd, roadStraight, corner, threeWay, fourWay;
+    public int rotationDegree = 0; // Default rotation degree for dead end, can be adjusted based on the default orientation of the dead end prefab.
     public void fixRoadAtPosition(PlacementManager placementManager,Vector3Int temporaryPosition)
     {
         //right, up, left , down
@@ -16,19 +17,21 @@ public class RoadFixer : MonoBehaviour
         {
             createDeadEnd(placementManager, result, temporaryPosition);
         }
+
         else if (roadCount == 2)
         {
-            if(createStraightRoad(placementManager, result, temporaryPosition))
+            if (createStraightRoad(placementManager, result, temporaryPosition))
             {
                 return;
             }
             createCorner(placementManager, result, temporaryPosition);
         }
 
-        else if(roadCount == 3)
+        else if (roadCount == 3)
         {
             createThreeWay(placementManager, result, temporaryPosition);
         }
+
         else
         {
             createFourWay(placementManager, result, temporaryPosition);
@@ -42,66 +45,70 @@ public class RoadFixer : MonoBehaviour
     }
 
     //[left, up, right, down] - [0, 1, 2, 3]
-    private void createThreeWay(PlacementManager placementManager, CellType[] result, Vector3Int temporaryPosition)
+    private void createThreeWay(PlacementManager placementManager, CellType[] result, Vector3Int position)
     {
         if (result[1] == CellType.Road && result[2] == CellType.Road && result[3] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, threeWay, Quaternion.identity);
+            rotationDegree = 0;
         }
         
         else if (result[2] == CellType.Road && result[3] == CellType.Road && result[0] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, threeWay, Quaternion.Euler(0,90,0));
+            rotationDegree = 90;
         }
 
         else if (result[3] == CellType.Road && result[0] == CellType.Road && result[1] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, threeWay, Quaternion.Euler(0, 180, 0));
+            rotationDegree = 180;
         }
 
         else if (result[0] == CellType.Road && result[1] == CellType.Road && result[2] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, threeWay, Quaternion.Euler(0, 270, 0));
+            rotationDegree = 270;
         }
+
+        placementManager.modifyStructureModel(position, threeWay, Quaternion.Euler(0, rotationDegree, 0));
     }
 
 
     //[left, up, right, down] - [0, 1, 2, 3]
-    private void createCorner(PlacementManager placementManager, CellType[] result, Vector3Int temporaryPosition)
+    private void createCorner(PlacementManager placementManager, CellType[] result, Vector3Int position)
     {
         if (result[1] == CellType.Road && result[2] == CellType.Road )
         {
-            placementManager.modifyStructureModel(temporaryPosition, corner, Quaternion.Euler(0, 90, 0));
+            rotationDegree = 90;
         }
 
         else if (result[2] == CellType.Road && result[3] == CellType.Road )
         {
-            placementManager.modifyStructureModel(temporaryPosition, corner, Quaternion.Euler(0, 180, 0));
+            rotationDegree = 180;
         }
 
         else if (result[3] == CellType.Road && result[0] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, corner, Quaternion.Euler(0, 270, 0));
+            rotationDegree = 270;
         }
 
         else if (result[0] == CellType.Road && result[1] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, corner, Quaternion.identity);
+            rotationDegree = 0;
         }
+
+        placementManager.modifyStructureModel(position, corner, Quaternion.Euler(0, rotationDegree, 0));
     }
 
     //[left, up, right, down] - [0, 1, 2, 3]
-    private bool createStraightRoad(PlacementManager placementManager, CellType[] result, Vector3Int temporaryPosition)
+    private bool createStraightRoad(PlacementManager placementManager, CellType[] result, Vector3Int position)
     {
         if (result[0] == CellType.Road && result[2] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, roadStraight, Quaternion.identity);
+            placementManager.modifyStructureModel(position, roadStraight, Quaternion.identity);
             return true;
         }
 
         else if (result[1] == CellType.Road && result[3] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, roadStraight, Quaternion.Euler(0,90,0));
+            placementManager.modifyStructureModel(position, roadStraight, Quaternion.Euler(0,90,0));
             return true;
         }
         return false;
@@ -109,26 +116,29 @@ public class RoadFixer : MonoBehaviour
 
 
     //[left, up, right, down] - [0, 1, 2, 3]
-    private void createDeadEnd(PlacementManager placementManager, CellType[] result, Vector3Int temporaryPosition)
+    private void createDeadEnd(PlacementManager placementManager, CellType[] result, Vector3Int position)
     {
+        
         if (result[1] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, deadEnd, Quaternion.Euler(0, 270, 0));
+            rotationDegree = 270;
         }
 
         else if (result[2] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, deadEnd, Quaternion.identity);
+            rotationDegree = 0;
         }
 
         else if (result[3] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, deadEnd, Quaternion.Euler(0, 90, 0));
+            rotationDegree = 90;
         }
 
         else if (result[0] == CellType.Road)
         {
-            placementManager.modifyStructureModel(temporaryPosition, deadEnd, Quaternion.Euler(0, 180, 0));
+            rotationDegree = 180;
         }
+
+        placementManager.modifyStructureModel(position, deadEnd, Quaternion.Euler(0, rotationDegree, 0));
     }
 }
