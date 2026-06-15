b1e923d07c6317675fc4e8c9661e69b0567cf218
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2026-06-15 17:27:56 +0700

    [feat] add new variable for future expansion

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -1,9 +1,39 @@
+using System;
 using UnityEngine;
 
 public class EconomyManager : MonoBehaviour
 {
-    public float budget;
+    public GoodsManager goodsManager;
+
+    private float budget;
     public float spending;
     public float income;
-    public float tax;
+    public float tax = 0.1f;
+
+    [Header("Simulation Settings")]
+    public float tickRateInSeconds = 2.0f;
+    private float tickTimer = 0.0f;
+
+    private void Start()
+    {
+        budget = 10000f;
+    }
+
+    private void Update()
+    {
+        tickTimer += Time.deltaTime;
+        if (tickTimer >= tickRateInSeconds)
+        {
+            runSimulationTick();
+            tickTimer = 0.0f;
+        }
+    }
+
+    private void runSimulationTick()
+    {
+        goodsManager.produceGoods();
+        goodsManager.sellGoods();
+        goodsManager.debugGoods();
+    }
+
 }
