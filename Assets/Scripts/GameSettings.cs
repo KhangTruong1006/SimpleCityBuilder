d2f62c7bae380a39562092a42e9b101e09003e96
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2026-06-18 20:36:57 +0700

    [feat] New settings for camera

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameSettings", menuName = "Game Settings")]
 public class GameSettings : ScriptableObject
 {
+    public Camera camera;
     public GridSettings gridSettings;
 
-    
+
+    [System.Serializable]
+    public class Camera
+    {
+        public float speed = 5.0f;
+    }
+
     [System.Serializable]
     public class GridSettings
     {
