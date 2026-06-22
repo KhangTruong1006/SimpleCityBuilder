c2e0985eae41efcd1f8003cb127a924ec18b963a
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2026-06-22 20:26:02 +0700

    [feat] new setting class of MasterSettings, including tickRateInSeconds

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameSettings", menuName = "Game Settings")]
 public class GameSettings : ScriptableObject
 {
+    public MasterSettings masterSettings;
     public Camera camera;
     public GridSettings gridSettings;
 
+
+    [System.Serializable]
+    public class MasterSettings
+    {
+        // Game speed (second)
+        public float tickRateInSeconds = 2.0f;
+    }
 
     [System.Serializable]
     public class Camera
     {
         public float speed = 5.0f;
     }
+
 
     [System.Serializable]
     public class GridSettings
