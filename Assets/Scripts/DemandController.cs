f9d2b86c4ecfb8cf36895a4e1f17a0034dcdd1c9
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2026-06-25 10:35:04 +0700

    [feat] new logic for handling RCI demands

--- Assets/Scripts/DemandController.cs
+++ Assets/Scripts/DemandController.cs
@@ -1,35 +1,138 @@
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.Port;
 
-public class DemandManager : MonoBehaviour
+public class DemandController : MonoBehaviour
 {
+    public PopulationManager PopulationManager;
+    public EconomyManager EconomyManager;
+    public ResourcesManager ResourcesManager;
+
     public float residentialDemand = 1.0f;
     public float commercialDemand = 0.0f;
     public float industrialDemand = 0.0f;
 
-    // Update is called once per frame
-    //void Update()
-    //{
+    private bool isInitialSeeding = true;
 
-    //}
 
+    public void updateDemand()
+    {
+        if (isInitialSeeding)
+        {
+            initialSeeding();
+        }
+
+        int population = PopulationManager.population;
+        int populationCapacity = PopulationManager.populationCapacity;
+        int jobCapacity = PopulationManager.jobCapacity;
+        int employedPopulation = PopulationManager.employedPopulation;
+
+        updateResidentialDemand(population, populationCapacity, employedPopulation, jobCapacity);
+        updateCommercialDemand(population);
+        updateIndustrialDemand(population, employedPopulation, jobCapacity);
+    }
+
+
+    private void initialSeeding()
+    {
+        if (PopulationManager.population > 5)
+        {
+            isInitialSeeding = false;
+        }
+
+        else
+        {
+            residentialDemand = 1.0f;
+            commercialDemand = 0.0f;
+            industrialDemand = 0.0f;
+        }
+    }
+
+
+    
+    // Residential Demand
+    private void updateResidentialDemand(int population, int capcity, int employedPopulation, int jobCapacity)
+    {
+        float housingVacancy = calculateHousingVacancy(population, capcity);
+        float jobVacancy = calculateJobVacancy(employedPopulation, jobCapacity);
+
+        // High when housing vacancy is low and available jobs are high
+        // Low when housing vacancy is high
+        float rawDemand = (1.0f - housingVacancy) + jobVacancy;
+        
+        residentialDemand = Mathf.Clamp01(rawDemand);
+    }
 
     private float calculateHousingVacancy(int population, int capcity)
     {
+        // New city - prevent 0 division
+        if(capcity <= 0)
+        {
+            return 0f;
+        }
+
         float vancancyRate = 1.0f - ((float)population / capcity);
         return vancancyRate;
     }
 
+    // Commercial Demand
+    private void updateCommercialDemand(int population)
+    {
+        if(population <= 0)
+        {
+            commercialDemand = 0f;
+            return;
+        }
+
+        float goodsDeficit = 1f - PopulationManager.goodsSatisfaction;
+        float rawDemand = (population * 0.05f) + goodsDeficit;
+
+        commercialDemand = Mathf.Clamp01(rawDemand);
+
+    }
+
     private float calculateJobVacancy(int employedPopulation, int jobCapacity)
     {
+        // New city - prevent 0 division
+        if (jobCapacity <= 0)
+        {
+            return 0f;
+        }
         float vancancyRate = 1.0f - ((float)employedPopulation / jobCapacity);
         return vancancyRate;
     }
 
-    private void calculateResidentialDemand(int population, int capcity, int employedPopulation, int jobCapacity)
+    // Industrial Demand
+    private void updateIndustrialDemand(int population, int employedPopulation, int jobCapacity)
     {
-        float housingVacancy = calculateHousingVacancy(population, capcity);
-        float jobVacancy = calculateJobVacancy(employedPopulation, jobCapacity);
-        
+        float unemploymentRate = calculateUnemploymentRate(population, employedPopulation);
+        float currentDemand = ResourcesManager.dynamicDemand;
+        float importReliance = calculateImportReliance(currentDemand);
+
+        float rawDemand = unemploymentRate + importReliance;
+
+        industrialDemand = Mathf.Clamp01(rawDemand);
     }
 
+    private float calculateImportReliance(float currentDemand)
+    {
+        if (currentDemand <= 0)
+        {
+            return 0f;
+        }
+
+        float importReliance = ResourcesManager.importDemand / currentDemand;
+        return importReliance;
+    }
+
+
+    private float calculateUnemploymentRate (int population, int employedPopulation)
+    {
+        if (population <= 0)
+        {
+            return 0f;
+        }
+
+        float unemployed = (float) (population - employedPopulation) / population;
+        return unemployed;
+    }
 }
