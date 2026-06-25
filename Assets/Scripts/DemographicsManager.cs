0ed2ada6799cc5dcf665049cb42e4f29c2e4c330
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2026-06-25 14:10:21 +0700

    [feat] new script for demographics

---
+++ Assets/Scripts/DemographicsManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DemographicsManager : MonoBehaviour
+{
+    // Children (0-18) - Young Adults (19 - 35) - Adutls (36 - 60) - Seniors (60+)
+    public int children;
+    public int youngAdults;
+    public int adults;
+    public int seniors;
+
+    private float preciseChildren;
+    private float preciseYoungAdults;
+    private float preciseAdults;
+    private float preciseSeniors;
+
+}
