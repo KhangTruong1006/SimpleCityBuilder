a54418faa511cc4ae0e309d2bf42a69f100b1b04
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2026-07-01 14:23:55 +0700

    [feat] new helper methods

--- Assets/Scripts/DemographicsManager.cs
+++ Assets/Scripts/DemographicsManager.cs
@@ -13,4 +13,32 @@ public class DemographicsManager : MonoBehaviour
     private float preciseAdults;
     private float preciseSeniors;
 
+    public float birthRate = 0.02f; 
+    public float agingRate = 0.05f; 
+    public float deathRate = 0.05f; 
+
+
+
+    // Helpers
+    public void updateTest()
+    {
+        children = convertFloatToInt(preciseChildren);
+        youngAdults = convertFloatToInt(preciseYoungAdults);
+        adults = convertFloatToInt(preciseAdults);
+        seniors = convertFloatToInt(preciseSeniors);
+    }
+
+    private int convertFloatToInt(float value)
+    {
+        return Mathf.FloorToInt(value);
+    }
+
+    private int getTotalPopulation()
+    {
+        return children + youngAdults + adults + seniors;
+    }   
+    public int getWorkForce()
+    {
+        return youngAdults + adults;
+    }
 }
