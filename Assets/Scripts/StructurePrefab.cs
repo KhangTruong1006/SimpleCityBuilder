360ab7364cbac164747019ba2ecb8d24cfe03b79
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2026-07-05 15:28:21 +0700

    [feat] new prefab for services

--- Assets/Scripts/StructurePrefab.cs
+++ Assets/Scripts/StructurePrefab.cs
@@ -8,6 +8,7 @@ public class StructurePrefab : ScriptableObject
     public CommericalPrefab[] commercialPrefabs;
     public IndustrialPrefab[] industrialPrefabs;
     public BigPrefab[] bigPrefabs;
+    public ServicePrefab[] servicePrefabs;
 }
 
 // === Interface ===
@@ -16,6 +17,8 @@ public interface IStructurePrefab
     public GameObject Prefab { get; }
     public float Weight { get; }
     public int Capacity { get; }
+
+    public int Cost { get; }
 }
 
 public interface IBusinessPrefab : IStructurePrefab
@@ -24,27 +27,38 @@ public interface IBusinessPrefab : IStructurePrefab
     public float GoodsUnitPerTick { get; } // Commerical: Sales per tick, Industrial: Produced Freight per tick
 }
 
+public interface IServicesPrefab
+{
+    public GameObject Prefab { get; }
+    public int Cost { get; }
+    public float ExpensePerTick { get; } // Unit: Money per tick
+}
+
 // === Struct ===
 
 [Serializable]
 public struct ResidentialPrefab : IStructurePrefab
 {
     public GameObject prefab;
+    public int cost;
 
     [Range(0f, 1f)]
     public float weight;
     public int populationCapacity;
 
+
     [Range(0f, 1f)]
     public GameObject Prefab => prefab;
     public float Weight => weight;
     public int Capacity => populationCapacity;
+    public int Cost => cost;
 }
 
 [Serializable]
 public struct IndustrialPrefab : IBusinessPrefab
 {
     public GameObject prefab;
+    public int cost;
     [Range(0f, 1f)]
     public float weight;
     public int workerCapacity;
@@ -58,12 +72,14 @@ public struct IndustrialPrefab : IBusinessPrefab
     public int Capacity => workerCapacity;
     public float InventoryCapacity => inventory;
     public float GoodsUnitPerTick => freightPerTick;
+    public int Cost => cost;
 }
 
 [Serializable]
 public struct CommericalPrefab : IBusinessPrefab
 {
     public GameObject prefab;
+    public int cost;
     [Range(0f, 1f)]
     public float weight;
     public int workerCapacity;
@@ -77,12 +93,14 @@ public struct CommericalPrefab : IBusinessPrefab
     public int Capacity => workerCapacity;
     public float InventoryCapacity => inventory;
     public float GoodsUnitPerTick => salesPerTick;
+    public int Cost => cost;   
 }
 
 [Serializable]
 public struct BigPrefab : IStructurePrefab
 {
     public GameObject prefab;
+    public int cost;
     [Range(0f, 1f)]
     public float weight;
     public int workerCapacity;
@@ -93,4 +111,19 @@ public struct BigPrefab : IStructurePrefab
     public GameObject Prefab => prefab;
     public float Weight => weight;
     public int Capacity => workerCapacity;
+    public int Cost => cost;
+}
+
+[Serializable]
+public struct ServicePrefab : IServicesPrefab
+{
+    public GameObject prefab;
+    public int cost;
+    [Min(0f)]
+    public float expensePerTick;
+
+
+    public GameObject Prefab => prefab;
+    public float ExpensePerTick => expensePerTick;
+    public int Cost => cost;
 }
