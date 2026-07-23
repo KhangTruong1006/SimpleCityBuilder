57ce4239090bc2577553f31d6c65bed1fa2cd32e
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2026-07-23 10:57:34 +0700

    [feat] create functions tp controll sliders

--- Assets/Scripts/Helper/SliderController.cs
+++ Assets/Scripts/Helper/SliderController.cs
@@ -1,6 +1,35 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SliderController : MonoBehaviour
-{
 
+{
+    [SerializeField] private GameSettings settings;
+    public EconomyManager economyManager;
+
+    public Slider taxSlider, timerSlider, residentialSlider, commercialSlider, industrialSlider;
+    [Header("Text")]
+    public TextMeshProUGUI taxTMP;
+
+    private void Start()
+    {
+        taxSlider.onValueChanged.AddListener((value)=> updateTaxOnSlider(value));
+    }
+
+
+    public void updateTaxOnSlider(float value)
+    {
+        economyManager.updateTax(value/100);
+        OnSliderChanged(taxTMP, value, "%");
+    }
+
+    public void OnSliderChanged(TextMeshProUGUI text, float value, string textString) {
+        text.text = value.ToString() + textString;
+    }
+
+    public void updateProgress(Slider slider, float progress)
+    {
+        slider.value = progress;
+    }
 }
